453bcbae9cf06a9d68240dac916686ca45b6930b
HP <sachdevamohita5@gmail.com>
2022-11-25 17:27:58 +0530

    first commit

---
+++ src/main/java/com/example/greetingcard/controller/GreetingController.java
@@ -0,0 +1,2 @@
+package com.example.greetingcard.controller;public class GreetingController {
+}
---
+++ src/test/java/com/example/greetingcard/GreetingCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greetingcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
